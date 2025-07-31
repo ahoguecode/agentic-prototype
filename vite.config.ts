cfe24e9bfc5f293d535738801596f73aa73a6a3c
Alex Hogue <ahogue@adobe.com>
2025-07-31 15:12:27 -0700

    pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,23 @@
 import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   return {
+    plugins: [react()],
+    base: '/agentic-prototype/',
     server: { 
       port: 5173 
     },
     build: {
       sourcemap: true,
       outDir: "./dist",
+      assetsDir: "assets",
+      rollupOptions: {
+        output: {
+          manualChunks: undefined,
+        }
+      }
     },
   };
 });
